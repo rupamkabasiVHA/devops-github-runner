47dc8219f0b76268ffce8843616c4fbc44013e3a
rupamkabasiVHA <76092811+rupamkabasiVHA@users.noreply.github.com>
2026-06-17 00:40:45 +1000

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,9 @@
-
 terraform {
   backend "s3" {
-    bucket         = "rupam-devops-terraform-state-bucket"
-    key            = "github-runner/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-locks"
+    bucket       = "rupam-terraform-state-bucket"
+    key          = "github-runner/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
